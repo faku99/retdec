459c0ecbf8cc0215f0ffc3638798905db166131e
Lucas Elisei <lucas.elisei@heig-vd.ch>
2018-05-15 13:15:25 +0000

    Added toolchain file

---
+++ toolchains/i686-linux-gnu.toolchain.cmake
@@ -0,0 +1,12 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_VERSION 1)
+
+set(CMAKE_C_COMPILER "i686-linux-gnu-gcc-7")
+set(CMAKE_CXX_COMPILER "i686-linux-gnu-g++-7")
+#set(CMAKE_AR "i686-linux-gnu-gcc-ar-7")
+#set(CMAKE_RANLIB "i686-linux-gnu-gcc-ranlib-7")
+
+#message(STATUS "Using C compiler from ${CMAKE_C_COMPILER}")
+#message(STATUS "Using C++ compiler from ${CMAKE_CXX_COMPILER}")
+#message(STATUS "Using ar from ${CMAKE_AR}")
+#message(STATUS "Using ranlib from ${CMAKE_RANLIB}")
